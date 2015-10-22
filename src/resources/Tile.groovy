f1a2342ff08140d80934eeeb3f69f3b4f10f82b9
omnijxs <juri.surakka@gmail.com>
2015-10-22 13:24:53 +0300

    Tile-objects have access to all popUnits via GameMap part deux.

--- src/resources/Tile.groovy
+++ src/resources/Tile.groovy
@@ -7,10 +7,6 @@ class Tile {
     
     GameMap map
     
-    public Tile(GameMap map){
-        this.map = map 
-    }
-    
     List<PopUnit> popUnitsOnTile(){
         return map.game.popUnits.findAll { it.tile == this }        
     }
