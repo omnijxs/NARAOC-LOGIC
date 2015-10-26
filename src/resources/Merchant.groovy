f5ef2be0fcf6e3ab6d42602078c11b0c2e5bd455
omnijxs <juri.surakka@gmail.com>
2015-10-26 14:45:39 +0200

    Added Merchant and Worker class as PopUnit subclasses.

---
+++ src/resources/Merchant.groovy
@@ -0,0 +1,13 @@
+package resources
+
+/**
+ * Created by Juri on 26.10.2015.
+ */
+
+class Merchant extends PopUnit {
+    
+    public Merchant(){
+        this.age = 0
+        this.priority = 2   
+    }
+}
---
+++ src/resources/Worker.groovy
@@ -0,0 +1,13 @@
+package resources
+
+/**
+ * Created by Juri on 26.10.2015.
+ */
+
+class Worker extends PopUnit {
+    
+    public Worker(){
+        this.age = 0
+        this.priority = 2   
+    }
+}
