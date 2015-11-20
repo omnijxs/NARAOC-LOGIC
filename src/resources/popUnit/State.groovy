d264a9b0b7d3e1c6b9fb6ef6292383ff906e39f2
omnijxs <juri.surakka@gmail.com>
2015-11-20 11:40:04 +0200

    Added lost State.groovy class.

---
+++ src/resources/popUnit/State.groovy
@@ -0,0 +1,17 @@
+package resources.popUnit
+
+import resources.common.Tile
+
+/**
+ * Created by Juri on 19.11.2015.
+ */
+class State {
+
+    Integer priority            // TODO this should not be here
+    
+    Integer age = 0
+    @Delegate Tile tile
+
+    // Race race
+    // Obedience obedience 
+}
