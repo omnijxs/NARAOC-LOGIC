a58a6e8ced237a576c7abda580e980dd173872c3
jxs <juri.surakka@gmail.com>
2015-12-01 20:51:26 +0200

    Initial implementation of popHubPopulationForFeeding query-method.

--- src/traits/PopUnitFinder.groovy
+++ src/traits/PopUnitFinder.groovy
@@ -9,8 +9,29 @@ import resources.popUnit.PopUnit
  */
 trait PopUnitFinder {
 
-    List<PopUnit> popHubPopulation(GameData gd, PopHub popHub){
-        return gd.popUnits.findAll { it.preferredHub == popHub || it.tile == popHub.tile }
+    /**
+     * The definition of popHubs population
+     *
+     * @param gd
+     * @param popHub
+     * @return
+     */
+    List<PopUnit> popHubPopulation(GameData gd){
+        return gd.popUnits.findAll { it.preferredHub == this || it.tile == this.tile }
     }
+
+    /**
+     * Finds those populationUnits which are to be fed by the popHub:
+     * Find all non-starving population units of the popHub
+     *
+     * @param gd
+     * @param popHub
+     * @return
+     */
+    List<PopUnit> popHubPopulationForFeeding(GameData gd, PopHub popHub){
+        return gd.popUnits.findAll { (it.preferredHub == this || it.tile == this.tile) && it.starving }
+    }
+
+    // [ ] PopUnitFinder should have two different population search methods: for production and for feeding. Also no popHub parameter required.
 
 }
