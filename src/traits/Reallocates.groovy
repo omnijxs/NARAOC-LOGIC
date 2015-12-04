03e270f021faf85b56f348d790d097b4523f1b30
omnijxs <juri.surakka@gmail.com>
2015-12-04 12:29:30 +0200

    Added a stub for Reallocates-trait.

---
+++ src/traits/Reallocates.groovy
@@ -0,0 +1,20 @@
+package traits
+
+/**
+ * Created by Juri on 04.12.2015.
+ */
+trait Reallocates {    
+  
+    def reallocate(){
+        
+        /** Deal with obedience */
+        
+        /** Create the new popUnit and remove this one from gameDate.popUnits */
+        
+        /** Deal with possible changes to tile */
+        
+        /** What will be the syntax to identify correct PopUnit-type? String? Enum? */
+        
+    }
+
+}
