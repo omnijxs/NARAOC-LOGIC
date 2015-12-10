6815af2513fe0a74eac7e6a105abc77da313f68d
omnijxs <juri.surakka@gmail.com>
2015-12-10 11:59:29 +0200

    Added another lost file to git.

---
+++ src/resources/popHub/PopHubOutput.groovy
@@ -0,0 +1,17 @@
+package resources.popHub
+
+/**
+ * Created by Juri on 17.11.2015.
+ */
+class PopHubOutput {
+
+    /** Use Enum/Integer map instead? */
+    Integer foodProduction
+    Integer workProduction
+    Integer tradeProduction
+    Integer surplusFood
+    Boolean closed
+
+    /** Or use a stack where this turns production is on top? */
+
+}
