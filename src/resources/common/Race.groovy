170439e31e4ef20ad1d4bf50ffe502b3b2d89fdc
omnijxs <juri.surakka@gmail.com>
2015-12-15 19:37:27 +0200

    Additions to Race-class.

--- src/resources/common/Race.groovy
+++ src/resources/common/Race.groovy
@@ -6,5 +6,16 @@ package resources.common
 class Race {
 
     /** Everything here will come from a config */
+    String label = ""
     Integer multiplicationRate = 0
+    Integer foodProductionBonus = 0
+    Integer workProductionBonus = 0
+    Integer tradeProductionBonus = 0
+    
+    /**
+     * Could be List noReallocationPenalty = ["ArmyUnit"]   
+     */    
+    Boolean noReallocationPenaltyForArmyUnit = false        
+    Boolean noReallocationPenaltyForMagicUnit = false
+    
 }
