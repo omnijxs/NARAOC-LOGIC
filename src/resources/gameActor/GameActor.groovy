4454d884bbcf946cf02764ac83b7cce658e6d6ab
jxs <juri.surakka@gmail.com>
2016-05-29 18:17:56 +0300

    FeedsArmyTest to FeedsArmySpec.

--- src/resources/gameActor/GameActor.groovy
+++ src/resources/gameActor/GameActor.groovy
@@ -7,8 +7,8 @@ import traits.Taxes
 /**
  * Created by Juri on 1.12.2015.
  */
-class GameActor implements FeedsArmy,
-                           Taxes {
+class GameActor implements FeedsArmy/*,
+                           Taxes*/ {
 
     Integer feedArmy(GameData gameData, Integer foodAmount){
         return feedsArmy(gameData.popUnits, this, foodAmount)
